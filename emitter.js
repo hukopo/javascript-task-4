c9a320acc7620cac974b8dce0741adc89e01da86
hukopo <hukopo@yandex.ru>
2018-11-04 20:07:28 +0500

    init

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
@@ -12,32 +12,56 @@ const isStar = true;
  */
 function getEmitter() {
     return {
-
-        /**
-         * Подписаться на событие
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         */
+        listener: {},
         on: function (event, context, handler) {
-            console.info(event, context, handler);
-        },
+            // handler.bind(context)();
+            if (this.listener[event] === undefined) {
+                this.listener[event] = { contexts: new Map() };
+            }
 
-        /**
-         * Отписаться от события
-         * @param {String} event
-         * @param {Object} context
-         */
+            const currentEventContexts = this.listener[event].contexts;
+
+            if (currentEventContexts.get(context) === undefined) {
+                currentEventContexts.set(context, []);
+            }
+
+            currentEventContexts.get(context).push(handler.bind(context));
+
+            return this;
+        },
         off: function (event, context) {
-            console.info(event, context);
-        },
+            Object.keys(this.listener).forEach(k => {
+                if (k.startsWith(event)) {
+                    this.listener[k].contexts.delete(context);
+                }
+            });
 
-        /**
-         * Уведомить о событии
-         * @param {String} event
-         */
+            return this;
+        },
         emit: function (event) {
-            console.info(event);
+            const eventSplit = event.split(/\./);
+            const events = [];
+            let currentEvent = '';
+            eventSplit.forEach(e => {
+                currentEvent += currentEvent.length === 0 ? e : '.' + e;
+                events.push(currentEvent);
+            });
+            events.reverse();
+            events.forEach(e => this.emit2(e));
+
+            return this;
+        },
+        emit2: function (event) {
+            if (!this.listener.hasOwnProperty(event)) {
+                return this;
+            }
+
+            this.listener[event].contexts
+                .forEach((v) => {
+                    v.forEach(f => f());
+                });
+
+            return this;
         },
 
         /**
@@ -49,6 +73,14 @@ function getEmitter() {
          * @param {Number} times – сколько раз получить уведомление
          */
         several: function (event, context, handler, times) {
+            this.on(event, context,
+                function () {
+                    if (this.callCount > this.maxCallCount) {
+                        return;
+                    }
+                    this.callCount++;
+                    handler(this.context);
+                }.bind({ callCount: 0, maxCallCount: times, context: context }));
             console.info(event, context, handler, times);
         },
 
